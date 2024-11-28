547f5f21ede7bcb2f073b4fb76d485c3cb8a2133
schnya <kda7sShump.aur@gmail.com>
2024-11-28 16:33:03 +0800

    Initial Commit

---
+++ ForMyEye/ForMyEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForMyEyeApp.swift
+//  ForMyEye
+//
+//  Created by 浅野竣弥 on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct ForMyEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForMyEyeUITests/ForMyEyeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ForMyEyeUITestsLaunchTests.swift
+//  ForMyEyeUITests
+//
+//  Created by 浅野竣弥 on 2024/11/28.
+//
+
+import XCTest
+
+final class ForMyEyeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
